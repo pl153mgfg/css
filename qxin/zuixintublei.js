0a55e3bee55e362a91db5f1510d48f32f1e21656
pl153mgfg <wocw09f8153@163.com>
2024-06-22 14:16:48 +0800

    Add files via upload

---
+++ qxin/zuixintublei.js
@@ -0,0 +1,14 @@
+  document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed("#index-xtips", {
+      strings: [
+        "公告：超强美化子主题，美化强势动力来自热爱，巨强大，新维度！",
+        "公告：按照用户要求，本站已取消频繁的通知提示",
+        "公告：根据用户建议已加入软件历史版本，其他功能在计划中..."
+      ],
+      startDelay: 100,
+      backDelay: 5000,
+      typeSpeed: 60,
+      loop: true,
+      backSpeed: 10,
+    });
+  });
